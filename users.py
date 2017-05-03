0af9115fda16bd124cc51bd7b563545f06a03842
quelleck <ethan.seyl@gmail.com>
2017-05-02 22:14:53 -0500

    add remote logic

--- users.py
+++ users.py
@@ -7,11 +7,18 @@ from time import sleep
 import logging.config
 import configparser
 import subprocess
+import base64
+import paramiko
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 config = configparser.ConfigParser()
 config.read('{}/config/config.ini'.format(dir_path))
+
+remote = True
+client = paramiko.SSHClient()
+client.load_system_host_keys()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 users = dict(
     zip((config['DEFAULT']['Users'].split(', ')), (config['HUE']['Devices']
@@ -53,25 +60,51 @@ def get_response(device):
     return output
 
 def does_file_exist(user):
-    my_file = Path("{}/users_home/{}_is.home".format(dir_path, user))
-    if my_file.is_file():
-        logging.debug("[users][does_file_exist] {} file exists.".format(user))
-        return True
+    if remote:
+        users_home = []
+        client.connect(config['DEFAULT']['RemoteServer'], username=config['DEFAULT']['RemoteUser'], password=config['DEFAULT']['RemotePw'])
+        stdin, stdout, stderr = client.exec_command('ls ~/home_automation/users_home/')
+        for line in stdout:
+            users_home.append(line.strip('\n'))
+        client.close()
+        print(users_home)
+        print(user)
+        if "{}_is.home".format(user) in users_home:
+            logging.debug("[users][does_file_exist] {} file exists remotely.".format(user))
+            return True
+    else:
+        my_file = Path("{}/users_home/{}_is.home".format(dir_path, user))
+        if my_file.is_file():
+            logging.debug("[users][does_file_exist] {} file exists.".format(user))
+            return True
 
 
 def create_file(user):
     if not does_file_exist(user):
-        open('{}/users_home/{}_is.home'.format(dir_path, user), 'w+b')
-        logging.debug("[users][create_file] {} file created.".format(user))
+        if remote:
+            client.connect(config['DEFAULT']['RemoteServer'], username=config['DEFAULT']['RemoteUser'], password=config['DEFAULT']['RemotePw'])
+            client.exec_command('touch ~/home_automation/users_home/{}_is.home'.format(user))
+            logging.debug("[users][create_file] {} file created remotely.".format(user))
+        else:
+            open('{}/users_home/{}_is.home'.format(dir_path, user), 'w+b')
+            logging.debug("[users][create_file] {} file created.".format(user))
 
 
 def delete_file(user):
     if does_file_exist(user):
-        try:
-            os.remove('{}/users_home/{}_is.home'.format(dir_path, user))
-            logging.debug("[users][delete_file] {} file deleted.".format(user))
-        except Exception as e:
-            logging.debug("[users][delete_file] {}".format(str(e)))
+        if remote:
+            try:
+                client.connect(config['DEFAULT']['RemoteServer'], username=config['DEFAULT']['RemoteUser'], password=config['DEFAULT']['RemotePw'])
+                client.exec_command('rm ~/home_automation/users_home/{}_is.home'.format(user))
+                logging.debug("[users][create_file] {} file created remotely.".format(user))
+            except Exception as e:
+                logging.debug("[users][delete_file] Remote: {}".format(str(e)))
+        else:
+            try:
+                os.remove('{}/users_home/{}_is.home'.format(dir_path, user))
+                logging.debug("[users][delete_file] {} file deleted.".format(user))
+            except Exception as e:
+                logging.debug("[users][delete_file] {}".format(str(e)))
     else:
         logging.debug(
             "[users][delete_file] {} file doesn't exist.".format(user))
